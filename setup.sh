7c5e06da5ece9f18483f1f76ef7cdb5b050b080b
harinadh12 <harinadhappidi@gmail.com>
2022-05-30 03:47:29 +0000

    shell and requirements.txt udpate to setup env

--- setup.sh
+++ setup.sh
@@ -1,20 +1,17 @@
-#install python 3.9
-sudo apt-get update -y
-sudo apt-get install python3.9
+# to get started with tensorflow federated run the following commands in order
 
-#change alternatives
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+#download minconda3 
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh 
 
-#check python version
-python --version
-#3.9.6
+#run shell file to install minconda3 base env
+bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
 
-# to update pip
-sudo apt install python3-pip
-sudo apt install python3.9-distutils
-python3 -m pip install --upgrade pip
+#restart the shell to activate base env
+
+#create new tff env for project
+conda create --name tff --clone base
+
+# install tensorflow-federated using pip
+pip install --upgrade tensorflow-federated
 
 
-pip install --quiet --upgrade tensorflow-federated
-pip install --quiet --upgrade nest-asyncio
