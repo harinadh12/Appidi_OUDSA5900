b3164a610b5da282fcb0845f82f7710adb5446b7
harinadh12 <harinadhappidi@gmail.com>
2022-07-13 20:20:41 +0000

    upload files from remote

--- data_extraction.py
+++ data_extraction.py
@@ -8,13 +8,18 @@ import pathlib
 import shutil
 import math
 from scipy.stats import mode
+from sklearn import feature_selection
 from sklearn.model_selection import train_test_split
-
+from sklearn.decomposition import PCA
+from joblib import dump, load 
 from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
+
 # add 
 # Tabulate Demographic charactersitics of participants
 # 
 
+LABEL_FREQUENCY = 700
 
 class ReadSubjectData:
     def __init__(self, file_path):
@@ -35,12 +40,22 @@ class ReadSubjectData:
     def get_signal_types(self):
         return self.data.get('signal',{}).keys()
 
-    #['ACC', 'ECG', 'EDA', 'EMG', 'Resp', 'Temp']
+    @staticmethod
+    def get_wrist_frequencies():
+        wrist_freq_dict = {}
+        wrist_freq_dict['acc_wrist_freq'] = 32
+        wrist_freq_dict['bvp_wrist_freq'] = 64
+        wrist_freq_dict['eda_wrist_freq'] = 4
+        wrist_freq_dict['temp_wrist_freq'] = 4
+
+        return wrist_freq_dict
+
+    # ['ACC', 'BVP', 'EDA', 'TEMP']
     def get_wrist_data(self):
         wrist_data = self.data.get('signal',{}).get('wrist',[])
         return wrist_data
     
-    # ['ACC', 'BVP', 'EDA', 'TEMP']
+    # ['ACC', 'ECG', 'EDA', 'EMG', 'Resp', 'Temp']
     def get_chest_data(self):
         chest_data = self.data.get('signal',{}).get('chest',[])
         return chest_data
@@ -51,12 +66,12 @@ class ReadSubjectData:
     def get_net_accel(self, data):
         return (data['ACC_x'] ** 2 + data['ACC_y'] ** 2 + data['ACC_z'] ** 2).apply(lambda x: np.sqrt(x))
     
-    def aggreage_features(self, feature, frequency=1400):
+    def aggregate_features(self, feature, frequency=700):
         mean_features = np.zeros(math.ceil(len(feature)/frequency))
         std_features = np.zeros(math.ceil(len(feature)/frequency))
         max_features = np.zeros(math.ceil(len(feature)/frequency))
         min_features = np.zeros(math.ceil(len(feature)/frequency))
-        sum_features = np.zeros(math.ceil(len(feature)/frequency))
+        # sum_features = np.zeros(math.ceil(len(feature)/frequency))
 
         idx =0
 
@@ -65,17 +80,59 @@ class ReadSubjectData:
             std_features[idx] = np.std(feature[i:i+frequency])
             max_features[idx] = np.amax(feature[i:i+frequency])
             min_features[idx] = np.amin(feature[i:i+frequency])
-            sum_features[idx] = np.sum(feature[i:i+frequency])
+            # sum_features[idx] = np.sum(feature[i:i+frequency])
             idx+=1
         
-        return np.column_stack((max_features, min_features, std_features, mean_features, sum_features))
+        return np.column_stack((max_features, min_features, std_features, mean_features))
         
-    def extract_chest_features(self, chest_data, indices):
+    def extract_chest_features(self, chest_data):
 
-        chest_data = chest_data[indices]
-        agg_data = np.apply_along_axis(self.aggreage_features, 0, chest_data)
+        # chest_data = chest_data[indices]
+        agg_data = np.apply_along_axis(self.aggregate_features, 0, chest_data)
         chest_data = agg_data.reshape(agg_data.shape[0],-1) # shape of (nrows, *4*n_columns)
         return chest_data
+
+    def extract_wrist_features(self, wrist_data_dict):
+        wrist_freq_dict = ReadSubjectData.get_wrist_frequencies()
+        reshape = lambda x: x.reshape(x.shape[0],-1)
+        agg_acc_wrist = reshape(np.apply_along_axis(self.aggregate_features,0,wrist_data_dict['ACC'],wrist_freq_dict['acc_wrist_freq']))
+        agg_bvp_wrist = reshape(np.apply_along_axis(self.aggregate_features,0,wrist_data_dict['BVP'],wrist_freq_dict['bvp_wrist_freq']))
+        agg_eda_wrist = reshape(np.apply_along_axis(self.aggregate_features,0,wrist_data_dict['EDA'],wrist_freq_dict['eda_wrist_freq']))
+        agg_temp_wrist = reshape(np.apply_along_axis(self.aggregate_features,0,wrist_data_dict['TEMP'],wrist_freq_dict['temp_wrist_freq']))
+
+        # print(agg_acc_wrist.shape, agg_bvp_wrist.shape, agg_eda_wrist.shape, agg_temp_wrist.shape)
+        agg_wrist_data = np.concatenate([agg_acc_wrist,agg_bvp_wrist,agg_eda_wrist,agg_temp_wrist], axis=1)
+        return agg_wrist_data
+    
+    def get_aggregate_labels(self):
+        idx=0
+        labels = self.get_labels()
+        new_labels = np.empty((labels.shape[0]//LABEL_FREQUENCY),)
+
+        for i in range(0, len(labels),LABEL_FREQUENCY):
+            new_labels[idx] = np.rint(np.mean(labels[i:i+700]))
+            idx +=1
+
+        return new_labels
+
+    def pca_dimension_reduction(self, features):
+        pca = PCA(n_components=8)
+        features = pca.fit_transform(features)
+        return features
+
+    def standardize_data(self, features, train=True):
+        
+        if train is True:
+            scaler = StandardScaler()
+            features = scaler.fit_transform(features)
+            with open('standard_scaler.pkl','w') as f:
+                dump(scaler,'scaler.gz')
+        else:
+            scaler = load('scaler.gz')
+            features = scaler.transform(features)
+
+        return features
+
 
 def get_pickle_files(dataset_path):
     """
@@ -89,74 +146,42 @@ def get_pickle_files(dataset_path):
 
     return pickle_files
 
+def create_train_test_data(features, labels):
+
+    dataset = {}
+    dataset['features'] = features
+    dataset['labels'] = labels
+
+
+
+    dataset['features'], dataset['labels'] = shuffle(dataset['features'], dataset['labels'])
+
+    train_dataset, test_dataset = collections.OrderedDict(), collections.OrderedDict()
+
+    train_dataset['features'], test_dataset['features'], train_dataset['labels'], test_dataset['labels'] = train_test_split(dataset['features'],dataset['labels'], test_size=0.3, random_state=1, stratify=dataset['labels'])
+
+
+    # train_dataset['features'] = obj.pca_dimension_reduction(train_dataset['features'])
+    train_dataset['features'] = obj.standardize_data(train_dataset['features'] , train=True)
+
+    # test_dataset['features'] = obj.pca_dimension_reduction(test_dataset['features'])
+    test_dataset['features'] = obj.standardize_data(test_dataset['features'] , train=False)
+
+    return train_dataset, test_dataset
+
 def create_aggregate_features(DATASET_PATH):
-
-    DATASET_PATH = "./WESAD/"
-    pickle_files = get_pickle_files(dataset_path=DATASET_PATH)
-    chest_data = {}
-    chest_labels = {}
-    for subject_no, file_path in pickle_files.items():
-
-        if os.path.exists(f'{DATASET_PATH}/{subject_no}_data.npy')  :
-            continue
-        
-        print(file_path)
-
-        # create an object for read_subject_data passing pickle file
-        obj = read_subject_data(
-            file_path=file_path
-        )
-
-        # get chest data from the subject object 
-        chest_data_dict = obj.get_chest_data()
-
-        # get labels(subject's activity) from subject object
-        chest_label = obj.get_labels()
-
-        # extract feature from chest data di t and aggregate them based on frequency window
-        chest_data = np.concatenate([chest_data_dict[key] for key in chest_data_dict], axis=1)
-
-        # Dimensions of different sensors used for chest data measurement
-        # 'ACC' : 3, 'ECG' 1: , 'EDA' : 1, 'EMG': 1, 'RESP': 1, 'Temp': 1  ===> Total dimensions : 8
-
-        # Labels --> 0 = not defined / transient, 1 = baseline, 2 = stress, 3 = amusement,
-        # 4 = meditation, 5/6/7 = should be ignored in this dataset
-
-        # Let's slice chest data for only labels --> 0,1,2,3,4 and ignore other labels in this experiment
-        chest_features = []
-        chest_labels = []
-        
-        for i in range(1,5):
-            # label_indices = np.where(np.logical_and(chest_label>0, chest_label<4))
-            label_indices = np.where(chest_label==i)
-            features = obj.extract_chest_features(chest_data=chest_data, indices=label_indices)
-            chest_features.append(features)
-            chest_labels.extend([i]*features.shape[0])
-        
-        
-        dataset = {}
-        dataset['features'] = np.vstack(chest_features)
-        dataset['labels'] = np.asarray(chest_labels)
-        dataset['features'], dataset['labels'] = shuffle(dataset['features'], dataset['labels'])
-
-        train_dataset, test_dataset = collections.OrderedDict(), collections.OrderedDict()
-
-        train_dataset['features'], test_dataset['features'], train_dataset['labels'], test_dataset['labels'] = train_test_split(dataset['features'],dataset['labels'], test_size=0.3, random_state=1)
-
-        with open(f'{DATASET_PATH}/{subject_no}_train_data.npy','wb') as f:
-            np.save(f, train_dataset)
-        
-        with open(f'{DATASET_PATH}/{subject_no}_test_data.npy','wb') as f:
-            np.save(f, test_dataset)
+    pass
 
 if __name__=="__main__":
 
-    DATASET_PATH = "./WESAD/"
+    DATASET_PATH = "../WESAD/"
     pickle_files = get_pickle_files(dataset_path=DATASET_PATH)
     chest_data = {}
     chest_labels = {}
-    for subject_no, file_path in pickle_files.items():
+    # mean_data = 0
 
+
+    for subject_no, file_path in pickle_files.items():
         if os.path.exists(f'{DATASET_PATH}/{subject_no}_train_data.npy')  :
             continue
         
@@ -170,38 +195,42 @@ if __name__=="__main__":
         # get chest data from the subject object 
         chest_data_dict = obj.get_chest_data()
 
+        #get wrist data from the subject object
+        wrist_data_dict = obj.get_wrist_data()
+
         # get labels(subject's activity) from subject object
-        chest_label = obj.get_labels()
+        labels = obj.get_labels()
 
         # extract feature from chest data di t and aggregate them based on frequency window
         chest_data = np.concatenate([chest_data_dict[key] for key in chest_data_dict], axis=1)
 
+
         # Dimensions of different sensors used for chest data measurement
         # 'ACC' : 3, 'ECG' 1: , 'EDA' : 1, 'EMG': 1, 'RESP': 1, 'Temp': 1  ===> Total dimensions : 8
+        
         # Labels --> 0 = not defined / transient, 1 = baseline, 2 = stress, 3 = amusement,
         # 4 = meditation, 5/6/7 = should be ignored in this dataset
 
         # Let's slice chest data for only labels --> 0,1,2,3,4 and ignore other labels in this experiment
-        chest_features = []
-        chest_labels = []
+
         
-        for i in range(1,4):
-            # label_indices = np.where(np.logical_and(chest_label>0, chest_label<4))
-            label_indices = np.where(chest_label==i)
-            features = obj.extract_chest_features(chest_data=chest_data, indices=label_indices)
-            chest_features.append(features)
-            chest_labels.extend([i]*features.shape[0])
+        chest_features = obj.extract_chest_features(chest_data=chest_data)
+        
+        wrist_features = obj.extract_wrist_features(wrist_data_dict)
         
         
-        dataset = {}
-        dataset['features'] = np.vstack(chest_features)
-        dataset['labels'] = np.asarray(chest_labels)
-        dataset['features'], dataset['labels'] = shuffle(dataset['features'], dataset['labels'])
+        labels = obj.get_aggregate_labels()
+        
+        chest_features = chest_features[np.where(np.logical_and(labels>0, labels<4))[0]]
+        
+        wrist_features = wrist_features[np.where(np.logical_and(labels>0, labels<4))[0]]
+        
+        features = np.concatenate([chest_features,wrist_features], axis=1)
+        labels = labels[np.where(np.logical_and(labels>0, labels<4))[0]]
 
-        train_dataset, test_dataset = collections.OrderedDict(), collections.OrderedDict()
+        train_dataset, test_dataset = create_train_test_data(features=features, labels=labels)
 
-        train_dataset['features'], test_dataset['features'], train_dataset['labels'], test_dataset['labels'] = train_test_split(dataset['features'],dataset['labels'], test_size=0.3, random_state=1)
-
+     
         with open(f'{DATASET_PATH}/{subject_no}_train_data.npy','wb') as f:
             np.save(f, train_dataset)
         
--- federate_data.py
+++ federate_data.py
@@ -7,130 +7,134 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.layers import Dense, InputLayer, Softmax
 from tensorflow.keras.models import Sequential
 
-DATASET_PATH = "./WESAD/"
-NUM_CLIENTS = 15
-NUM_EPOCHS = 5
-BATCH_SIZE = 20
-SHUFFLE_BUFFER = 200
-PREFETCH_BUFFER = 10
-NUM_ROUNDS = 20
+
+# DATASET_PATH = "./WESAD/"
+# NUM_CLIENTS = 15
+# NUM_EPOCHS = 5
+# BATCH_SIZE = 20
+# SHUFFLE_BUFFER = 200
+# PREFETCH_BUFFER = 10
+# NUM_ROUNDS = 20
 
 
-def read_data(DATASET_PATH):
+# def read_data(DATASET_PATH):
 
-    train_dataset_dict = collections.OrderedDict()
-    test_dataset_dict = collections.OrderedDict()
+#     train_dataset_dict = collections.OrderedDict()
+#     test_dataset_dict = collections.OrderedDict()
 
-    for root, dirnames, filenames in os.walk(DATASET_PATH, topdown=True):
-        for file_name in filenames :
+#     for root, dirnames, filenames in os.walk(DATASET_PATH, topdown=True):
+#         for file_name in filenames :
             
-            if file_name.endswith('.npy'):
-                subject_no = file_name.split('_')[0]
-                file_path = os.path.join(root,file_name)
+#             if file_name.endswith('.npy'):
+#                 subject_no = file_name.split('_')[0]
+#                 file_path = os.path.join(root,file_name)
 
-                if os.path.exists(file_path):           
-                    data = np.load(file_path, allow_pickle=True)
-                    print(f'Number of examples in subject {subject_no}: ',data.item().get('features').shape[0])
-                    if file_name.endswith('train_data.npy'):
-                        train_dataset_dict[subject_no] = collections.OrderedDict((('features', data.item().get('features')),('labels',data.item().get('labels'))))
-                    elif file_name.endswith('test_data.npy'):
-                        test_dataset_dict[subject_no] = collections.OrderedDict((('features', data.item().get('features')),('labels',data.item().get('labels'))))
+#                 if os.path.exists(file_path):           
+#                     data = np.load(file_path, allow_pickle=True)
+#                     print(f'Number of examples in subject {subject_no}: ',data.item().get('features').shape[0])
+#                     if file_name.endswith('train_data.npy'):
+#                         train_dataset_dict[subject_no] = collections.OrderedDict((('features', data.item().get('features')),('labels',data.item().get('labels'))))
+#                     elif file_name.endswith('test_data.npy'):
+#                         test_dataset_dict[subject_no] = collections.OrderedDict((('features', data.item().get('features')),('labels',data.item().get('labels'))))
 
-    return train_dataset_dict, test_dataset_dict
+#     return train_dataset_dict, test_dataset_dict
 
-def convert_to_client_dataset(train_data, test_data):
-    tff_train_dataset = tff.simulation.datasets.TestClientData(train_data)
-    tff_test_dataset = tff.simulation.datasets.TestClientData(test_data)
+# def convert_to_client_dataset(train_data, test_data):
+#     tff_train_dataset = tff.simulation.datasets.TestClientData(train_data)
+#     tff_test_dataset = tff.simulation.datasets.TestClientData(test_data)
 
-    return tff_train_dataset, tff_test_dataset
+#     return tff_train_dataset, tff_test_dataset
 
-# update this method afterwards
-def plot_each_subject():
+# # update this method afterwards
+# def plot_each_subject():
 
-    # Number of examples per layer for a sample of clients
-    f = plt.figure(figsize=(12, 7))
-    f.suptitle('Label Counts for a Sample of Clients')
-    for i in range(6):
-    client_dataset = tff_dataset.create_tf_dataset_for_client(
-        tff_dataset.client_ids[i])
-    plot_data = collections.defaultdict(list)
-    for example in client_dataset:
-        # Append counts individually per label to make plots
-        # more colorful instead of one color per plot.
-        print(example)
-        label = example['labels'].numpy()
-        plot_data[label].append(label)
-    plt.subplot(2, 3, i+1)
-    plt.title('Client {}'.format(i))
-    for j in range(10):
-        plt.hist(
-            plot_data[j],
-            density=False,
-            bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+#     # Number of examples per layer for a sample of clients
+#     f = plt.figure(figsize=(12, 7))
+#     f.suptitle('Label Counts for a Sample of Clients')
+#     for i in range(6):
+#     client_dataset = tff_dataset.create_tf_dataset_for_client(
+#         tff_dataset.client_ids[i])
+#     plot_data = collections.defaultdict(list)
+#     for example in client_dataset:
+#         # Append counts individually per label to make plots
+#         # more colorful instead of one color per plot.
+#         print(example)
+#         label = example['labels'].numpy()
+#         plot_data[label].append(label)
+#     plt.subplot(2, 3, i+1)
+#     plt.title('Client {}'.format(i))
+#     for j in range(10):
+#         plt.hist(
+#             plot_data[j],
+#             density=False,
+#             bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-def preprocess_fn(dataset):
+# def preprocess_fn(dataset):
 
-  def batch_format_fn(element,epsilon=1e-8):
-    """Flatten a batch `features` and return the features as an `OrderedDict`."""
+#   def batch_format_fn(element,epsilon=1e-8):
+#     """Flatten a batch `features` and return the features as an `OrderedDict`."""
 
-    mean, variance = tf.nn.moments(element['features'], axes=[0])
-    element['features']  = (element['features']  - mean) / tf.sqrt(variance + epsilon) 
+#     mean, variance = tf.nn.moments(element['features'], axes=[0])
+#     element['features']  = (element['features']  - mean) / tf.sqrt(variance + epsilon) 
     
-    return collections.OrderedDict(
-        x=tf.reshape(element['features'], [-1, 32]),
-        y=tf.reshape(element['labels'], [-1, 1]))
+#     return collections.OrderedDict(
+#         x=tf.reshape(element['features'], [-1, 32]),
+#         y=tf.reshape(element['labels'], [-1, 1]))
 
-  return dataset.repeat(NUM_EPOCHS).shuffle(SHUFFLE_BUFFER, seed=1).batch(
-      BATCH_SIZE).map(batch_format_fn).prefetch(PREFETCH_BUFFER)
+#   return dataset.repeat(NUM_EPOCHS).shuffle(SHUFFLE_BUFFER, seed=1).batch(
+#       BATCH_SIZE).map(batch_format_fn).prefetch(PREFETCH_BUFFER)
 
 
-def make_federated_data(client_data, client_ids):
-  return [
-      preprocess_fn(client_data.create_tf_dataset_for_client(x))
-      for x in client_ids
-  ]
+# def make_federated_data(client_data, client_ids):
+#   return [
+#       preprocess_fn(client_data.create_tf_dataset_for_client(x))
+#       for x in client_ids
+#   ]
 
-def create_keras_model():
+# def create_keras_model():
 
-  return tf.keras.models.Sequential([
-      tf.keras.layers.InputLayer(input_shape=(32,)),
-      # tf.keras.layers.Dense(15, kernel_initializer='glorot_uniform'),
-      # tf.keras.layers.Dense(20, kernel_initializer='zeros'),
-      tf.keras.layers.Dense(10,kernel_initializer='glorot_uniform', ),
-      # kernel_regularizer=tf.keras.regularizers.l2(.0001)),
-      tf.keras.layers.Softmax()
+#   return tf.keras.models.Sequential([
+#       tf.keras.layers.InputLayer(input_shape=(32,)),
+#       # tf.keras.layers.Dense(15, kernel_initializer='glorot_uniform'),
+#       # tf.keras.layers.Dense(20, kernel_initializer='zeros'),
+#       tf.keras.layers.Dense(10,kernel_initializer='glorot_uniform', ),
+#       # kernel_regularizer=tf.keras.regularizers.l2(.0001)),
+#       tf.keras.layers.Softmax()
       
-  ])
+#   ])
 
 
-def model_fn():
-  # We _must_ create a new model here, and _not_ capture it from an external
-  # scope. TFF will call this within different graph contexts.
-  keras_model = create_keras_model()
-  return tff.learning.from_keras_model(
-      keras_model,
-      input_spec=data.element_spec,
-      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+# def model_fn():
+#   # We _must_ create a new model here, and _not_ capture it from an external
+#   # scope. TFF will call this within different graph contexts.
+#   keras_model = create_keras_model()
+#   return tff.learning.from_keras_model(
+#       keras_model,
+#       input_spec=data.element_spec,
+#       loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+#       metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-def build_fed_avg_process(federated_train_data):
+# def build_fed_avg_process(federated_train_data):
 
-    iterative_process = tff.learning.build_federated_averaging_process(
-    model_fn,
-    client_optimizer_fn=lambda: tf.keras.optimizers.Adam(learning_rate=0.001),
-    server_optimizer_fn=lambda: tf.keras.optimizers.Adam(learning_rate=0.01))
-    state = iterative_process.initialize()
+#     iterative_process = tff.learning.build_federated_averaging_process(
+#     model_fn,
+#     client_optimizer_fn=lambda: tf.keras.optimizers.Adam(learning_rate=0.001),
+#     server_optimizer_fn=lambda: tf.keras.optimizers.Adam(learning_rate=0.01))
+#     state = iterative_process.initialize()
     
-    for round_num in range(2, NUM_ROUNDS):
-        state, metrics = iterative_process.next(state, federated_train_data)
-        print('round {:2d}, metrics={}'.format(round_num, metrics))
+#     for round_num in range(2, NUM_ROUNDS):
+#         state, metrics = iterative_process.next(state, federated_train_data)
+#         print('round {:2d}, metrics={}'.format(round_num, metrics))
 
     
-def run_fed_evaluation(tff_test_data):
-    evaluation = tff.learning.build_federated_evaluation(model_fn)
-    sample_clients = tff_test_data.client_ids[:NUM_CLIENTS]
-    federated_test_data = make_federated_data(tff_test_data, sample_clients)
+# def run_fed_evaluation(tff_test_data):
+#     evaluation = tff.learning.build_federated_evaluation(model_fn)
+#     sample_clients = tff_test_data.client_ids[:NUM_CLIENTS]
+#     federated_test_data = make_federated_data(tff_test_data, sample_clients)
 
-    model_weights = iterative_process.get_model_weights(state)
-    test_metrics = evaluation(model_weights, federated_test_data)
-    test_metrics
+#     model_weights = iterative_process.get_model_weights(state)
+#     test_metrics = evaluation(model_weights, federated_test_data)
+#     test_metrics
+
+
+
